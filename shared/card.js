aebaf97aabb77c0c048f16fdb64333e1432cc7bc
abdulroheem <ayanwunmiabdulroheem@gmail.com>
2023-02-02 02:26:58 +0100

    feat: card styles added

--- shared/card.js
+++ shared/card.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import {View, Text, StyleSheet, Button} from 'react-native'
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <View>
-            <Text>This is a card</Text>
+        <View style={styles.card}>
+            <View style={styles.cardContent}>
+                {props.children}
+            </View>
         </View>
     )
 }
 
 export default Card
+
+
+const styles = StyleSheet.create({
+    card: {
+
+    },
+    cardContent: {
+        
+    }
+});
